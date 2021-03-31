7325428102aaf64f521452d406a03055f00c43a9
Faiyyazauxy <faiyyazk@auxesisinfotech.com>
2021-03-31 17:49:31 +0530

    handle null context in getsize

--- example/android/app/src/main/java/com/yourcompany/example/MainActivity.java
+++ example/android/app/src/main/java/com/yourcompany/example/MainActivity.java
@@ -1,7 +1,6 @@
 package com.yourcompany.example;
 
 import android.os.Bundle;
-import io.flutter.app.FlutterActivity;
 import io.flutter.embedding.android.FlutterActivity;
 
 public class MainActivity extends FlutterActivity {
